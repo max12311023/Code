b28624cfc3bba4785104e702feb53aa11b394f3c
max <max12311023@gmail.com>
2017-07-10 09:43:43 +0800

    node.js

---
+++ not.js/loop.js
@@ -0,0 +1,11 @@
+sum =0;
+i=1;
+while(i<=10){
+	sum= sum+i;
+	console.log("i=",i," sum=",sum);
+	i = i +1;
+}
+console.log("-----For------------");
+for (i=1;i<=10;i++) {
+  console.log("i=", i);
+}
